08e1409e65fbf4725c04150fc40596f135d18cc7
Brendan Bohn <brendanbohn@comcast.net>
2023-12-04 09:52:21 -0800

    Day 1 Problem 1 working

--- 2023/day-4/problem-1.js
+++ 2023/day-4/problem-1.js
@@ -1,30 +1,26 @@
-import { match } from "assert";
 import fs from "fs";
 
-const inputLines = fs
-  .readFileSync("./example-1.txt", "utf8")
-  .trim()
-  .split("\n");
+const inputLines = fs.readFileSync("./input.txt", "utf8").trim().split("\n");
 
 let totalPoints = 0;
 
 for (let i = 0; i < inputLines.length; i++) {
+  let cardPoints = 0;
   let matchedWinningNumbers = [];
+
   const card = inputLines[i];
   const cardNumbers = card
     .split(":")[1]
     .split("|")
     .map((numbers) => numbers.trim().split(" "));
-  const winningNumbers = cardNumbers[0];
-  const candidateNumbers = cardNumbers[1];
+  const winningNumbers = cardNumbers[0].filter((num) => num !== "");
+  const candidateNumbers = cardNumbers[1].filter((num) => num !== "");
 
   for (let j = 0; j < winningNumbers.length; j++) {
     if (candidateNumbers.includes(winningNumbers[j])) {
       matchedWinningNumbers.push(winningNumbers[j]);
     }
   }
-
-  let cardPoints = 0;
 
   if (matchedWinningNumbers.length > 1) {
     cardPoints = 2 ** (matchedWinningNumbers.length - 1);
